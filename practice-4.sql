52bf0e3d4bddbeccf16b33b696109762bd748ef5
premaltailor <premal.tailor@kellogg.northwestern.edu>
2022-01-15 20:05:08 +0000

    Finished Week 2 Practice Exercise

--- practice-4.sql
+++ practice-4.sql
@@ -4,4 +4,6 @@
 -- | 272      |
 -- +----------+
 
-
+SELECT COUNT(neighborhood)
+FROM listings
+WHERE neighborhood = "Lincoln Park";
--- practice-5.sql
+++ practice-5.sql
@@ -11,5 +11,8 @@
 -- | Private room in residential home    | 12       |
 -- | Private room in townhouse           | 3        |
 -- +-------------------------------------+----------+
-
+SELECT property_type, COUNT(*), neighborhood
+FROM listings 
+WHERE neighborhood = "Hyde Park"
+GROUP BY property_type;
 
